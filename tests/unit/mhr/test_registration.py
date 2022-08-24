e19f4138f1382b7fb9985bc675cd35db680c72ea
Doug Lovett <doug@diamante.ca>
2022-08-24 08:57:27 -0700

    Lint fix.
    Signed-off-by: Doug Lovett <doug@diamante.ca>

--- tests/unit/mhr/test_registration.py
+++ tests/unit/mhr/test_registration.py
@@ -133,7 +133,7 @@ def test_registration(desc, valid, mhr, status, ref, decv, haso, hasl, hasd, has
     if desc == 'Invalid doc id too long':
         data['documentId'] = data.get('documentId') + '9'
     elif desc == 'Invalid attention too long':
-        data['attentionReference'] =  LONG_ATTENTION_REF
+        data['attentionReference'] = LONG_ATTENTION_REF
 
     is_valid, errors = validate(data, 'registration', 'mhr')
 
